30471701175bd257e72ddbede91c88248ee2cf96
Van Hon <nguyenvanhon.tk4@gmail.com>
2024-11-13 00:41:53 +0700

    up

--- src/pages/NewPage.tsx
+++ src/pages/NewPage.tsx
@@ -1,13 +1,14 @@
 import { faker } from '@faker-js/faker';
-import { Box, Container } from "@mui/material";
+import { AppBar, Box, Container, Typography, useTheme } from "@mui/material";
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
-import { FixedSizeList as List } from "react-window";
-import InfiniteLoader from "react-window-infinite-loader";
-import AutoSizer from "react-virtualized-auto-sizer";
 import { makeStyles } from "@material-ui/core/styles";
 
+import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
+import { TreeItem } from '@mui/x-tree-view/TreeItem';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 interface SimpleCardProps {
    index: number;
    style: React.CSSProperties;
@@ -21,24 +22,56 @@ interface dataSimple {
    url: string
 }
 
-// Component SimpleCard giả lập
-const SimpleCard:React.FC<SimpleCardProps> = ({ index, style, data }) => (
-   <Box sx={{ ...style, padding: 1, border: '1px solid #ccc', marginBottom: '10px' }}>
-      <img src={data?.img} width='100%' alt='img' height='150px'/>
-      {data.text}
-   </Box>
- );
- 
- const useStyles = makeStyles({
-   container: {
-     position: "relative"
-   }
- });
 
- const ListContainer:React.FC<any> = props => {
-   const classes = useStyles();
-   return <Container style={{width:'100%'}} className={classes.container} {...props} />;
+interface TabPanelProps {
+   children?: React.ReactNode;
+   dir?: string;
+   index: number;
+   value: number;
+ }
+ 
+ function TabPanel(props: TabPanelProps) {
+   const { children, value, index, ...other } = props;
+ 
+   return (
+     <div
+       role="tabpanel"
+       hidden={value !== index}
+       id={`full-width-tabpanel-${index}`}
+       aria-labelledby={`full-width-tab-${index}`}
+       {...other}
+     >
+       {value === index && (
+         <Box sx={{ p: 3 }}>
+           <Typography>{children}</Typography>
+         </Box>
+       )}
+     </div>
+   );
+ }
+ 
+ function a11yProps(index: number) {
+   return {
+     id: `full-width-tab-${index}`,
+     'aria-controls': `full-width-tabpanel-${index}`,
+   };
+ }
+
+const SimpleCard: React.FC<SimpleCardProps> = ({ index, style, data }) => {
+   // Kiểm tra nếu dữ liệu không hợp lệ thì không render SimpleCard
+   if (!data || !data.text || !data.img) {
+     return null; // Trả về null nếu không có dữ liệu hợp lệ
+   }
+ 
+   return (
+     <Box sx={{ ...style, padding: 1, border: '1px solid #ccc', marginBottom: '10px' }}>
+       <img src={data.img} width="100%" alt="img" height="150px" />
+       {data.text}
+     </Box>
+   );
  };
+
+
  function getRandomElement<T>(arr: T[]): T {
    const randomIndex = Math.floor(Math.random() * arr.length);
    return arr[randomIndex];
@@ -46,7 +79,12 @@ const SimpleCard:React.FC<SimpleCardProps> = ({ index, style, data }) => (
 const NewPage: React.FC = () => {
    const { t } = useTranslation();
    const [data, setData] = React.useState<dataSimple[]>([]);
-
+   const theme = useTheme();
+   const [value, setValue] = React.useState(0);
+ 
+   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+     setValue(newValue);
+   };
    const [linkPlay, setLinkPlay] = React.useState<string>('https://www.youtube.com/embed/W3skwsfBKoI?si=4xs1YUV8RpTtntH8');
 
    const stringsImg = ["https://img.freepik.com/free-vector/hand-drawn-english-school-background_23-2149482186.jpg?semt=ais_hybrid", 
@@ -59,6 +97,7 @@ const NewPage: React.FC = () => {
       "https://www.youtube.com/embed/ach22N49DjE?si=_AoFSiya2d5PxdpT"];
    // Load dữ liệu khi component mount
    useEffect(() => {
+      loadMoreItems(1,4);
       const newData = Array.from({ length: 500 }).map(() => ({
          id: 'id_unit',
          text: faker.lorem.sentence(),
@@ -77,7 +116,7 @@ const NewPage: React.FC = () => {
           for (let idx = startIndex; idx < stopIndex; idx++) {
             newData[idx] = {
               id: 'id_unit',
-              text: faker.lorem.sentence(),
+              text: faker.lorem.sentence() || 'topic',
               img: getRandomElement(stringsImg),
               url: getRandomElement(stringVideo),
             };
@@ -88,6 +127,8 @@ const NewPage: React.FC = () => {
       });
     };
    
+
+
    const onClickBox =(item: dataSimple)=>{
       setLinkPlay(item?.url)
    }
@@ -99,9 +140,10 @@ const NewPage: React.FC = () => {
                display: "grid",
                gridTemplateColumns: {
                   xs: '1fr', 
-                  sm: '4fr 1fr', 
+                  sm: '3fr 1fr', 
                 },
                padding: "0px 10px",
+
             }}
          >
             <Box
@@ -130,39 +172,81 @@ const NewPage: React.FC = () => {
             <Box
                sx={{
                   width: "100%",
-                  paddingLeft:'20px'
+                  paddingLeft:'20px',
+                  height:'65vh',
+                  maxHeight: '65vh',
+                  overflow: 'hidden', /* Ẩn thanh cuộn mặc định */
+                  '&:hover': {
+                     overflow: 'scroll', /* Hiển thị thanh cuộn khi hover */
+                  },
+                  transition: 'all 0.3s ease', /* Thêm hiệu ứng chuyển tiếp */
+                  
                }}
             >
-               <AutoSizer>
-                  {({ height, width }: { height: number; width: number }) => (
-                  <InfiniteLoader
-                     isItemLoaded={isItemLoaded}
-                     itemCount={data.length}
-                     loadMoreItems={loadMoreItems}
-                  >
-                     {({ onItemsRendered, ref }) => (
-                        <List
-                        className="List"
-                        height={height}
-                        width='300px'
-                        itemCount={data.length}
-                        itemSize={230}
-                        itemData={data}
-                        innerElementType={ListContainer}
-                        onItemsRendered={onItemsRendered}
-                        ref={ref}
-                        >
-                        {({ index, style }) => (
-                           <Box onClick={()=>onClickBox(data[index])}>
-                              <SimpleCard index={index} style={style} data={data[index]} />
+               <SimpleTreeView>
+                  <TreeItem itemId="grid" label="Data Grid">
+                   {data.map((item, index) => (
+                        <TreeItem itemId={`grid-item-1-${index}`} key={`grid-item-${index}`} label={item.text}>
+                           <Box onClick={() => onClickBox(item)}>
+                              <SimpleCard index={index} style={{}} data={item} />
                            </Box>
-                        )}
-                        </List>
-                     )}
-                  </InfiniteLoader>
-                  )}
-               </AutoSizer>
+                        </TreeItem>
+                     ))}
+                  </TreeItem>
+                  <TreeItem itemId="pickers" label="Date and Time Pickers">
+                     {data.map((item, index) => (
+                        <TreeItem itemId={`grid-item-2-${index}`} key={`grid-item-${index}`} label={item.text}>
+                           <Box onClick={() => onClickBox(item)}>
+                              <SimpleCard index={index} style={{}} data={item} />
+                           </Box>
+                        </TreeItem>
+                     ))}
+                  </TreeItem>
+                  <TreeItem itemId="charts" label="Charts">
+                     {data.map((item, index) => (
+                        <TreeItem itemId={`grid-item-3-${index}`} key={`grid-item-${index}`} label={item.text}>
+                           <Box onClick={() => onClickBox(item)}>
+                              <SimpleCard index={index} style={{}} data={item} />
+                           </Box>
+                        </TreeItem>
+                     ))}
+                  </TreeItem>
+                  <TreeItem itemId="tree-view" label="Tree View">
+                     {data.map((item, index) => (
+                        <TreeItem itemId={`grid-item-4-${index}`} key={`grid-item-${index}`} label={item?.text }>
+                           <Box onClick={() => onClickBox(item)}>
+                              <SimpleCard index={index} style={{}} data={item} />
+                           </Box>
+                        </TreeItem>
+                     ))}
+                  </TreeItem>
+                  </SimpleTreeView>
             </Box>
+         </Box>
+         <Box sx={{ bgcolor: 'background.paper', width: '100%' }}>
+            <AppBar position="static">
+            <Tabs
+               value={value}
+               onChange={handleChange}
+               indicatorColor="secondary"
+               textColor="inherit"
+               variant="fullWidth"
+               aria-label="full width tabs example"
+            >
+               <Tab label="Item One" {...a11yProps(0)} />
+               <Tab label="Item Two" {...a11yProps(1)} />
+               <Tab label="Item Three" {...a11yProps(2)} />
+            </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0} dir={theme.direction}>
+               custom here 
+            </TabPanel>
+            <TabPanel value={value} index={1} dir={theme.direction}>
+               Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2} dir={theme.direction}>
+               Item Three
+            </TabPanel>
          </Box>
       </Container>
    );
